39922985092add9105043bee4e8a8e15de6b0fbc
kaiokada <okadak12@gmail.com>
2020-12-03 22:37:28 -0500

    Missed some files in initial commit.

--- GameDemo/Game1.cs
+++ GameDemo/Game1.cs
@@ -1,44 +1,48 @@
-﻿using System;
-using System.IO;
-using System.Text.Json;
-using GameDemo.Characters;
-using GameDemo.Events;
+﻿using GameDemo.Characters;
+using GameDemo.Engine;
+using GameDemo.Managers;
+using GameDemo.Startup;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
+
 namespace GameDemo
 {
+    
     public class Game1 : Game
     {
         private GraphicsDeviceManager Graphics;
         private SpriteBatch SpriteBatch;
         private MainCharacter MainCharacter;
-        private EventManager EventManager;
+
         private const int DEFAULT_WIDTH = 1280;
         private const int DEFAULT_HEIGHT = 780;
 
+        private static bool IsQuitting = false;
+
+        private static GameEngine MainEngine = null;
 
         public Game1()
         {
             Graphics = new GraphicsDeviceManager(this);
+            MainEngine = new GameEngine();
+
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+
         }
 
         protected override void Initialize()
         {
+
             Graphics.PreferredBackBufferWidth = DEFAULT_WIDTH;
             Graphics.PreferredBackBufferHeight = DEFAULT_HEIGHT;
             Graphics.ApplyChanges();
 
             MainCharacter = new MainCharacter();
-            String path = Path.Combine(Content.RootDirectory, "json-sample.txt");
-            String Text = File.ReadAllText(path);
-            AllEventDialogue AllEventDialogue = JsonSerializer.Deserialize<AllEventDialogue>(Text);
-            EventDialogue EventDialogue = AllEventDialogue.AllEvents["eventString"][0];
+            IsMouseVisible = true;
 
-            EventManager = new EventManager(this.MainCharacter, EventDialogue, this.Content);
+            MainEngine.Push(new StartupManager(), false, false);
 
             base.Initialize();
         }
@@ -46,6 +50,7 @@ namespace GameDemo
         protected override void LoadContent()
         {
             SpriteBatch = new SpriteBatch(GraphicsDevice);
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -53,19 +58,29 @@ namespace GameDemo
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            EventManager.Update(gameTime);
+            if (IsQuitting) Exit();
+
+            MainEngine.Update(gameTime, MainCharacter, Content);
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             SpriteBatch.Begin();
-            EventManager.Draw(SpriteBatch, Graphics);
+
+            MainEngine.Draw(SpriteBatch, Graphics);
+
             SpriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+        public static void QuitGame()
+        {
+            IsQuitting = true;
+        }
+        
     }
 }
